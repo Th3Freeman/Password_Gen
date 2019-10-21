e1b357be53254b17dbeb2498632ac56a2664ef09
Th3freeman <thefrman345@gmail.com>
2019-10-20 20:33:56 -0400

    played around with the divs on index.html and replaced the form with a <p> to be edited with js. added vars and prompts in js and created an array of those vars for later reference

---
+++ assets/java.js
@@ -0,0 +1,10 @@
+var charNum = prompt("How long would you like your password? Select a number between 8 and 128")
+var specialChar = prompt("Would you like any special characters?")
+var upperCase = prompt("Uppercase Letters?")
+var lowerCase = prompt("Lowercase Letters?")
+var numbers = prompt("Numbers?")
+
+var feedBack = [charNum, specialChar, upperCase, lowerCase, numbers]
+
+console.log(feedBack)
+
