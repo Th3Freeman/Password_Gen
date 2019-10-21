af5337e2ce1d9b4a39b5469122ea7313ee913d80
Th3freeman <thefrman345@gmail.com>
2019-10-20 23:12:51 -0400

    finished up the if/else statements and tried to work in an option for a non answer scenario but was having trouble and moved on to trying to get the button to generate the password that is being output

--- assets/java.js
+++ assets/java.js
@@ -1,13 +1,20 @@
-alert("Answer the prompts to generate a password. If you leave a prompt blank that value will be randomly be chosen")
-var charNum = prompt("How long would you like your password? Select a number between 8 and 128")
+alert("Answer the prompts to generate a password. If you choose an invalid number of characters a valid number will be chosen at random. If you dont any attributes then it assumes you want everything.")
+var charNum = prompt("How long would you like your password? Select a number between 8 and 128.")
 var specialChar = confirm("Would you like any special characters?")
 var upperCase = confirm("Uppercase Letters?")
 var lowerCase = confirm("Lowercase Letters?")
 var numbers = confirm("Numbers?")
-
 var feedBack = [charNum, specialChar, upperCase, lowerCase, numbers]
 
 console.log(feedBack)
+
+if (charNum>128) {
+    charNum = Math.floor(Math.random() * 128) + 8
+}
+
+else if (charNum<8) {
+    charNum = Math.floor(Math.random() * 128) + 8
+};
 
 if (specialChar) {
     var specials = "!@#$%^&*()_+"
@@ -15,7 +22,7 @@ if (specialChar) {
 
 else {
     specials = ""
-}
+};
 
 if (upperCase) {
     var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -23,13 +30,37 @@ if (upperCase) {
 
 else {
     uppers = ""
+};
+
+if (lowerCase) {
+    var lowers = "abcdefghijklmnopqrstuvwxyz"
 }
+
+else {
+    lowers = ""
+};
+
+if (numbers) {
+    var nums = "0123456789"
+}
+
+else {
+    nums = ""
+};
+
+// if (specials="", uppers="", lowers="",nums="") {
+//     var randos="!@#$%^&*()_+ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+// }
+
+// else {
+//     randos=""
+// };
 
 
 
 function makeid(charNum) {
     var result = '';
-    var characters = specials+uppers;
+    var characters = specials+uppers+lowers+nums;
     var charactersLength = characters.length;
     for (var i = 0; i < charNum; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -38,3 +69,7 @@ function makeid(charNum) {
 }
 
 console.log(makeid(charNum));
+
+function genrator() {
+    document.getElementById("password_output") = makeid(charNum);
+  }
