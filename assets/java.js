d377dbdb8eb0f769e2a0256081885e0ef2c47386
Th3freeman <thefrman345@gmail.com>
2019-10-20 21:40:07 -0400

    added a function to generate a random string based on user input and tied it to a few if/else statements to test functionality and was pleasantly surprised that it works

--- assets/java.js
+++ assets/java.js
@@ -1,10 +1,40 @@
+alert("Answer the prompts to generate a password. If you leave a prompt blank that value will be randomly be chosen")
 var charNum = prompt("How long would you like your password? Select a number between 8 and 128")
-var specialChar = prompt("Would you like any special characters?")
-var upperCase = prompt("Uppercase Letters?")
-var lowerCase = prompt("Lowercase Letters?")
-var numbers = prompt("Numbers?")
+var specialChar = confirm("Would you like any special characters?")
+var upperCase = confirm("Uppercase Letters?")
+var lowerCase = confirm("Lowercase Letters?")
+var numbers = confirm("Numbers?")
 
 var feedBack = [charNum, specialChar, upperCase, lowerCase, numbers]
 
 console.log(feedBack)
 
+if (specialChar) {
+    var specials = "!@#$%^&*()_+"
+}
+
+else {
+    specials = ""
+}
+
+if (upperCase) {
+    var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+}
+
+else {
+    uppers = ""
+}
+
+
+
+function makeid(charNum) {
+    var result = '';
+    var characters = specials+uppers;
+    var charactersLength = characters.length;
+    for (var i = 0; i < charNum; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+console.log(makeid(charNum));
